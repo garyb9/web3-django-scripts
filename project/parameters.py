e5e3fbada915472648e8f6991a9b0a76d96f1a8e
Gary Berkinfand <gberkinfand@gmail.com>
2021-07-04 16:25:59 +0300

    adding parameters and local_settings (gitignored)

---
+++ project/parameters.py
@@ -0,0 +1,23 @@
+# Parameters
+
+# ------------------------------------------------------
+# --------------------- ETH Params ---------------------
+# ------------------------------------------------------
+
+ETH_ADDR_LEN            = 40
+
+ETH_HEX_ADDR_LEN        = 42
+
+MAINNET_INFURA_HTTP     = "https://mainnet.infura.io/v3/"
+
+MAINNET_INFURA_WS       = "wss://mainnet.infura.io/ws/v3/"
+
+RINKEBY_WS              = "wss://rinkeby-light.eth.linkpool.io/ws"
+
+OPENSEA_API_URL         = "https://api.opensea.io/api/v1/assets"
+
+LIMIT_OPENSEA           = "30"  # default is 20, caps at 50
+
+ALCHEMY_API_HTTP        = "https://eth-mainnet.alchemyapi.io/v2/"
+
+ALCHEMY_API_WS          = "wss://eth-mainnet.ws.alchemyapi.io/v2/"
--- project/settings.py
+++ project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,14 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Parameters
+from .parameters import *
+
+
+# For development & production
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
